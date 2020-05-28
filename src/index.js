5de30e48d4718781b14e99de8f6e019cf0d4955e
Jamie Berrier <berrier.jamie@gmail.com>
2020-05-28 18:29:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("#create-task-form").addEventListener("submit", function(event) {
+    const tasks = document.getElementById("tasks");
+    let newTask = document.createElement('li');
+    newTask.textContent = document.getElementById('new-task-description').value;
+
+    tasks.appendChild(newTask);
+    event.preventDefault();
+  }, false);
 });
